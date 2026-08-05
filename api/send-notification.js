a2085d57f051977b6d3f084261c96f2b25d4d500
mohammedhamada1907-commits <293922830+mohammedhamada1907-commits@users.noreply.github.com>
2026-08-05 04:48:34 +0300

    Update OneSignal authorization key in notification API

--- api/send-notification.js
+++ api/send-notification.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": "Key aycgp63cwevpmwwsnt4ldp57u"
+        "Authorization": "Key os_v2_app_ioudvkwwnjctzhd3f6peqxxkigaycgp63cwevpmwwsnt4ldp57u32yzuwwhvjmxk243ztnzxovk7rfbmpw364pgiyuc3stpxk3loeiy"
       },
       body: JSON.stringify(req.body)
     });
